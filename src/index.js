a6b0831a42ddf01cfd49268f8fe8c541dd399ed2
dilipmeena2001 <dilipmeena2001@gmail.com>
2026-07-31 14:43:10 +0530

    asyncHandler, ApiResponse and ApiError wrapper is built in utils

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 // require("dotenv").config({path:"./env"})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
 
 dotenv.config({
     path: "./env"
 })
 
+const port = process.env.PORT || 8000
 
-connectDB()
+connectDB().then(
+    ()=>{
+        app.listen(port, ()=>{
+            console.log(`Server is running at port : ${port}`)
+        })
+    }
+).catch((error)=>{
+    console.log("Database Connection failed !! :", error)
+})
 
 
 
